bce7eabbc12d025dcdeef63fd932a174e0b3830d
Zachary Estes <zachestes187@gmail.com>
2022-10-13 16:40:33 -0700

    login servlet for app

--- Project1/src/main/java/com/revatureproj/servlets/LoginServlet.java
+++ Project1/src/main/java/com/revatureproj/servlets/LoginServlet.java
@@ -10,7 +10,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 
@@ -29,25 +31,48 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-      //  Users user = ud.getByLogin(username);
-      //  List<users> userList = ud.getByLogin();
+        List<Users> users = ud.getAllUsers();
 
         HashMap<String, Object> credentials = mapper.readValue(req.getInputStream(), HashMap.class);
 
         String provUserName = (String) credentials.get("username");
         String provPassword = (String) credentials.get("password");
 
-        Users users = ud.getByLogin(provUserName);
-
         for (Users user: users){
+            if (provUserName.equals(user.getUsername()) && provPassword.equals(user.getPassword())){
+                System.out.println("[LOG] - found user!");
 
+                HttpSession session = req.getSession();
+                session.setAttribute("auth-user", user);
+
+                resp.setStatus(204);
+                return;
+            }
         }
+
+        resp.setStatus(400);
+        resp.setContentType("application/json");
+
+        HashMap<String, Object> errorMessage = new HashMap<>();
+
+        errorMessage.put("Status code", 400);
+        errorMessage.put("Message", "No user found with provided credentials");
+        errorMessage.put("Timestamp", LocalDateTime.now().toString());
+
+        resp.getWriter().write(mapper.writeValueAsString(errorMessage));
     }
 
 
     //logging out
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+        HttpSession session = req.getSession(false);
+
+        if(session != null){
+            System.out.println(session.getAttribute("auth-user"));
+            session.invalidate();
+        }
+
+        resp.setStatus(204);
     }
 }
